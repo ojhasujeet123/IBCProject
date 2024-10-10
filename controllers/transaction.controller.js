26d5c291504c9e5adea918c43e530aaa9dea42cd
kuldeep349 <349kuldeep@gmail.com>
2024-10-10 16:27:33 +0530

    ojha code merge

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -12,123 +12,19 @@ const db = require("../models");
 const Transactions = db.transactions;
 const CurrentBlock = db.currentBlock;
 const cron = require('node-cron');
-const Holder = require('../models/holder.model');
-
-//code on live 
-// let Scanblock = async( ) => {
-//     const latest = await web3.eth.getBlockNumber()  
-//     console.log('its latest block ' , latest) 
-//     let lastBlock = await CurrentBlock.findOne({});
-//     let i =  lastBlock.blockNumber;
-//     let endBlock = parseInt(latest);// > (parseInt(i)+ parseInt(200)) ? latest :(i + parseInt(i)+ parseInt(200) ) ;   //(parseFloat(lastBlock.blockNumber) + parseFloat(50)) 
-//     // endBlock = (endBlock - i) > 200 ? (parseInt(i) + parseInt(200)) : endBlock;
-//     console.log('now we scan from ' + i + ' to '+ endBlock) 
-//     for(i; i <  endBlock; i++){ 
-//         console.log('its i ', i )
-//         let block = await blockInfo(i) 
-        
-        
-//         if(block.transactions && block.transactions.length > 0){ 
-//             for(s = 0; s <  block.transactions.length ; s++){ 
-//                 let transaction = block.transactions[s]
-//                 console.log('main transacrtion is ' , transaction) 
-//                 let functionName = '';
-//                 if(transaction.input == '0x'){
-//                     functionName = ''
-//                 }else{ 
-//                     const decodeRes = decoder.decodeData(transaction.input);
-//                     console.log("decodeRes>>>>>>>>>>>>>",decodeRes);
-                    
-//                     let functionsMethods = []; 
-//                     for(k = 0; k <  decodeRes.types.length; k++){ 
-//                         functionsMethods.push(decodeRes.types[k] + ' ' + decodeRes.names[k])
-//                     } 
-//                     functionName = decodeRes.method +'('+functionsMethods.join()+')'
-//                 }
-//                 let transactionReceipt = await web3.eth.getTransactionReceipt(transaction.hash);//eth.getTransactionReceipt("0x636b6d5ede1a24d9efeff079a8da5c0b22692419ec01f9afa4653fcc871a00cf")
-//                 console.log('its transaction receipt ' , transactionReceipt.logs)
-
-//                 console.log('its transaction receipt ' , transactionReceipt)
-//                 let transactionData = {
-//                     blockNumber:parseInt(transaction.blockNumber),
-//                     timeStamp:Number(block.timestamp),
-//                     blockHash:transaction.blockHash,
-//                     from:transaction.from,
-//                     to:transaction.to,
-//                     hash:transaction.hash,
-//                     value:transaction.value , //web3.utils.fromWei(transaction.value, 'ether'),
-//                     gas:transaction.gas, 
-//                     gasPrice:transaction.gasPrice, 
-//                     nonce : transaction.nonce,
-//                     transactionIndex : parseInt(transaction.transactionIndex),
-//                     isError:0,
-//                     txreceipt_status : 1,
-//                     input : transaction.input,
-//                     contractAddress:transactionReceipt.contractAddress,
-//                     cumulativeGasUsed : parseInt(transactionReceipt.cumulativeGasUsed),
-//                     gasUsed : parseInt(transactionReceipt.gasUsed),
-//                     confirmations : 0,
-//                     functionName :functionName,
-//                 }
-
-
-//                 if(transaction.input != '0x'){
-//                     if(transaction.input.substr(0,10) == '0xa9059cbb'){
-//                         const result =decoder.decodeData(transaction.input);// web3.utils.numberToHex(transaction.input); 
-//                         transactionData.contractAddress = transactionReceipt.logs[0].address;// transaction.input.substr(10,64)
-//                         transactionData.contractValue = web3.utils.fromWei(transaction.input.substr(74), 'ether');
-//                         transactionData.methodId = transaction.input.substr(0,10)
-//                         transactionData.input = transaction.input
-//                         // console.log('check for contract address ',web3.utils.bytesToHex(transaction.input.substr(10,64)))
-//                     }else{
-//                         console.log('its contract creation',transaction.type)
-//                         transactionData.methodId = '0x';
-//                     }
-//                 }else{
-//                     transactionData.methodId = transaction.input;
-//                     console.log('its normal transaction ')
-//                 } 
-//                 console.log('transaction data ' , transactionData)
-//                 await checkUserBalance(transactionData.from)
-//                 if(transactionData.to)
-//                     await checkUserBalance(transactionData.to)
-
-//                 insertTransactions(transactionData); 
-                
-//             }  
-//         }
-        
-//     }   
-
-//     CurrentBlock.updateOne({ "_id": lastBlock._id }, {
-//         "blockNumber": endBlock,
-//     }).then(data => {
-//                 console.log('update data ', data)
-//             }).catch(err => {
-//                 console.log('update err ', err)
-//             });
- 
-// }
-
-/*
-sujeet ojha 06-09-24
-purpose - bypassing the contract address
-*/ 
-
 
 let Scanblock = async( ) => {
-    const latest = await web3.eth.getBlockNumber()  
+    
+    const latest = await web3.eth.getBlockNumber() 
     console.log('its latest block ' , latest) 
     let lastBlock = await CurrentBlock.findOne({});
-    // let i =  lastBlock.blockNumber;
-    let i =932611
+    let i =  lastBlock.blockNumber;
     let endBlock = parseInt(latest);// > (parseInt(i)+ parseInt(200)) ? latest :(i + parseInt(i)+ parseInt(200) ) ;   //(parseFloat(lastBlock.blockNumber) + parseFloat(50)) 
     // endBlock = (endBlock - i) > 200 ? (parseInt(i) + parseInt(200)) : endBlock;
     console.log('now we scan from ' + i + ' to '+ endBlock) 
     for(i; i <  endBlock; i++){ 
         console.log('its i ', i )
         let block = await blockInfo(i) 
-        
         
         if(block.transactions && block.transactions.length > 0){ 
             for(s = 0; s <  block.transactions.length ; s++){ 
@@ -139,8 +35,6 @@ let Scanblock = async( ) => {
                     functionName = ''
                 }else{ 
                     const decodeRes = decoder.decodeData(transaction.input);
-                    console.log("decodeRes>>>>>>>>>>>>>",decodeRes);
-                    
                     let functionsMethods = []; 
                     for(k = 0; k <  decodeRes.types.length; k++){ 
                         functionsMethods.push(decodeRes.types[k] + ' ' + decodeRes.names[k])
@@ -149,14 +43,12 @@ let Scanblock = async( ) => {
                 }
                 let transactionReceipt = await web3.eth.getTransactionReceipt(transaction.hash);//eth.getTransactionReceipt("0x636b6d5ede1a24d9efeff079a8da5c0b22692419ec01f9afa4653fcc871a00cf")
                 console.log('its transaction receipt ' , transactionReceipt.logs)
-
-                console.log('its transaction receipt ' , transactionReceipt)
                 let transactionData = {
                     blockNumber:parseInt(transaction.blockNumber),
                     timeStamp:Number(block.timestamp),
                     blockHash:transaction.blockHash,
                     from:transaction.from,
-                    to:transaction.to===null?transactionReceipt.contractAddress:transaction.to,
+                    to:transaction.to,
                     hash:transaction.hash,
                     value:transaction.value , //web3.utils.fromWei(transaction.value, 'ether'),
                     gas:transaction.gas, 
@@ -166,7 +58,6 @@ let Scanblock = async( ) => {
                     isError:0,
                     txreceipt_status : 1,
                     input : transaction.input,
-                    contractAddress:transactionReceipt.contractAddress,
                     cumulativeGasUsed : parseInt(transactionReceipt.cumulativeGasUsed),
                     gasUsed : parseInt(transactionReceipt.gasUsed),
                     confirmations : 0,
@@ -211,12 +102,9 @@ let Scanblock = async( ) => {
             });
  
 }
-
-
-
 cron.schedule('* * * * *', () => {
     // if(process.env.BASE_URL == 'https://blcscan.org/'){
-        // Scanblock(); 
+        Scanblock(); 
     // }
     
 });
@@ -241,9 +129,7 @@ let blockInfo = async (blockNumber) => {
      
     
 }
-// Scanblock() 
-
-
+Scanblock()
 
 
 
@@ -252,9 +138,6 @@ let checkUserBalance = async (address) => {
     updateUserBalanxe(address,balance)
     console.log('address ' , address , balance);
 }
-
-
-
 
 let updateUserBalanxe = async (address, balance) => {
     let addressExist = await Holders.findOne({ address: address });
@@ -288,21 +171,22 @@ let updateUserBalanxe = async (address, balance) => {
   }
 
 
-// const updateHoldersBalances = async () => {
-//     const transactions = await Transactions.find({}).lean();
+const updateHoldersBalances = async () => {
+    const transactions = await Transactions.find({}).lean();
   
-//     for (const transaction of transactions) {
-//       try {
-//         const fromAddress = transaction.from;
-//         const toAddress = transaction.to;
+    for (const transaction of transactions) {
+      try {
+        const fromAddress = transaction.from;
+        const toAddress = transaction.to;
   
-//         await checkUserBalance(fromAddress);
-//         await checkUserBalance(toAddress);
-//       } catch (err) {
-//         console.error(err);
-//       }
-//     }
-//   };
+        await checkUserBalance(fromAddress);
+        await checkUserBalance(toAddress);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
 
 
 
@@ -384,7 +268,22 @@ getAllTransactions: async (req, res, next) => {
 
 
 
+//Transaction By hash
 
+    // getTransactionByHash: async (req, res, next) => {
+    //     try {
+    //         const transactionOfSingleUser = await Transactions.findOne({ hash: req.params.hash });
+
+    //         if (!transactionOfSingleUser) {
+    //             return res.status(404).json({ message: "Transaction not found for the given hash" });
+    //         }
+
+    //         res.status(200).json({ transactionOfSingleUser });
+    //     } catch (error) {
+    //         console.error(error);
+    //         next(error);
+    //     }
+    // },
 
 
     getTransactionByHash: async (req, res, next) => {
@@ -416,39 +315,33 @@ getAllTransactions: async (req, res, next) => {
             const { page = 1, limit = 10 } = req.query;
             const skip = (page - 1) * limit;
             
-            const [txnCount, transactions] = await Promise.all([
-                Transactions.find({ $or: [{ from: req.params.address }, { to: req.params.address }] }).countDocuments(),
-                Transactions.find({
-                    $or: [{ from: req.params.address }, { to: req.params.address }]
-                }).sort({ createdAt: -1 }).skip(skip).limit(limit)
-            ]);
-    
+            const [txnCount,transactions]=await Promise.all([
+                await Transactions.find({$or: [{ from: req.params.address }, { to: req.params.address }]}).countDocuments(),
+                await Transactions.find({
+                        $or: [{ from: req.params.address }, { to: req.params.address }]
+                    }).sort({createdAt:-1}).skip(skip).limit(limit)
+            ])
+
             if (!transactions || transactions.length === 0) {
                 return res.status(404).json({ message: "Transactions not found for this address" });
             }
             
-
-            const addressBalance =await Holder.findOne({address:req.params.address },{etherBalance:1,contractAddress:0})
-
-
             const formattedTransactions = transactions.map(transaction => {
+                // Assuming createdAt field is a Date object, you can format it using toLocaleString or any other formatting method
                 return {
-                   ...transaction._doc,
-                    createdAt: getElapsedTime(transaction.createdAt) ,
-                    addressBalance: addressBalance.etherBalance
-
+                    ...transaction._doc,
+                    createdAt: getElapsedTime(transaction.createdAt) // Assuming formatDate is your formatting function
                 };
             });
     
-  
-    
-            res.status(200).json({ txnCount, transactionByAddress: formattedTransactions});
+            res.status(200).json({ txnCount,transactionByAddress: formattedTransactions });
         } catch (error) {
             console.error(error);
             next(error);
         }
-    },
-
+    },  
+    
+    // Example formatting function
 
     
 };
@@ -456,8 +349,5 @@ getAllTransactions: async (req, res, next) => {
 
 
 
-module.exports={...transactionController}
-
-
-
-
+module.exports={updateHoldersBalances,...transactionController}
+onController}
